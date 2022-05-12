9f3358f10a9d85ff4a7b8fb30b341059e7052ef0
jo0oy <rkfkrh07@naver.com>
2022-05-12 12:03:07 +0900

    파일 정리

--- src/main/java/jpabook/jpastore/domain/order/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package jpabook.jpastore.domain.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-//    List<Order> findAllWithOption(OrderSearch orderSearch);
-}
